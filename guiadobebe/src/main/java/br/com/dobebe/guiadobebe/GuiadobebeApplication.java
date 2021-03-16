dbaba8657ae9e19ac80c13509687decedadfbfb4
AlessandroBartsch <49681821+AlessandroBartsch@users.noreply.github.com>
2021-03-16 19:23:23 -0300

    Base do projeto
    Estrutura base para inicio do projeto, com apenas o que foi baixado do initializr.

---
+++ guiadobebe/src/main/java/br/com/dobebe/guiadobebe/GuiadobebeApplication.java
@@ -0,0 +1,13 @@
+package br.com.dobebe.guiadobebe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuiadobebeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuiadobebeApplication.class, args);
+	}
+
+}
---
+++ guiadobebe/src/test/java/br/com/dobebe/guiadobebe/GuiadobebeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.dobebe.guiadobebe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuiadobebeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
